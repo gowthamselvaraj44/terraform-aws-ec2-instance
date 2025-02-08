3d0a865bdd5118eae117cb850945de9905f36db7
gowthamselvaraj44 <75294091+gowthamselvaraj44@users.noreply.github.com>
2025-02-08 17:29:33 +0530

    Update main.tf

--- terraform -modules/main.tf
+++ terraform -modules/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-   region = "us-east-2"
+   region = "us-east-2" #replace
 }
 
 module "ec2_Instance" {
   source = "./module/ec2_Instance"
-  ec2_ami = "ami-0cb91c7de36eed2cb"
-  ec2_instance_type = "t2.micro"
+  ec2_ami = "ami-XXXXXXXXXXXX" #replace
+  ec2_instance_type = "t2.micro" #replace
 }
